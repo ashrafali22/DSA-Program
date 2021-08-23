da00bd1f646367b7d3489e87056acd6334a8ef6b
Ashraf <ashrafali.11810784@gmail.com>
2021-08-23 10:22:58 +0530

    program upload

---
+++ first_n_table.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int x=1;x<=n;x++)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			cout<<(x*i)<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
